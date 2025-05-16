e6410e074e3a1815e5962dc9992671b70b421890
Nico van Ommen <nicovanommen.nvo@gmail.com>
2025-05-16 18:26:51 +0200

    Made succesfull connection with mqtt server

--- include/config-example.hpp
+++ include/config-example.hpp
@@ -6,6 +6,13 @@
 
 
 /**
+ * @brief General settings for the device goes here
+ * 
+ */
+const char DEVICE_NAME[]                = "robot_xxxx";
+
+
+/**
  * @param Here goes all the settings for configuring the parameters of FreeRTOS
  *  
  */
@@ -18,6 +25,18 @@ const uint32_t MIN_TASK_STACK_SIZE      = 10000;
  */
 const char WIFI_SSID[]                  = "<Your wifi SSID>";
 const char WIFI_PASSWORD[]              = "<Your wifi password>";
+
+
+/**
+ * @brief Settings for connecting with MQTT
+ * 
+ */
+const char MQTT_CLIENT_ID[]             = "robot_xxxx";
+const bool MQTT_CLIENT_ANONYMOUS        = true;
+const char MQTT_USER[]                  = "<MQTT username here>";
+const char MQTT_PASS[]                  = "<MQTT password here>";
+const char MQTT_SERVER[]                = "<MQTT server address here>";
+const int MQTT_PORT                     = 1883;
 
 
 /**
--- src/network.cpp
+++ src/network.cpp
@@ -1,14 +1,36 @@
 #include "network.hpp"
 #include <Arduino.h>
 #include <WiFi.h>
+#include <WiFiClient.h>
+#include <time.h>
 #include "config.hpp"
+#include "PubSubClient.h"
+
+
+// Static globals needed for this file
+WiFiClient      wifi_client;
+PubSubClient    mqtt_client;
+bool            mqtt_connected;
+
+
+/**
+ * @brief Callback function to capture all mqtt data from the server
+ * 
+ * @param topic
+ * @param payload
+ * @param length
+ */
+void mqtt_callback(const char* topic, byte* payload, unsigned int length)
+{
+
+}
 
 
 /**
  * @brief Function to detect the macaddress of the ESP32
  * 
  */
-void readMacAddress(){
+void read_mac_address(){
     uint8_t baseMac[6];
     
     // Get MAC address of the WiFi station interface
@@ -22,15 +44,18 @@ void readMacAddress(){
 
 
 /**
- * @brief Function to connect to the internet and process any network activity
+ * @brief Function to initialize the wifi network
  * 
- * @param param
  */
-void network_task(void *param)
-{ 
+void init_Wifi() 
+{
     // Before the loop can start, it is required that a stable network connection is needed!
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    // Sync time with the time server
+    Serial.println("Trying to sync time!");
+    configTime(0, (3600 * 2), "ntppool1.time.nl", "ntppool2.time.nl");
 
     Serial.print("Wifi is trying to connect to the network: ");
 
@@ -38,11 +63,47 @@ void network_task(void *param)
         Serial.print(".");
         delay(500);
     }
+
     Serial.println("");
     Serial.println("Wifi connected to network!");
+}
+
+
+/**
+ * @brief Function to initialize the mqtt server before it can be used
+ * 
+ */
+void init_mqtt()
+{
+    mqtt_connected = false;
+
+    mqtt_client.setClient(wifi_client);
+    mqtt_client.setServer(MQTT_SERVER, MQTT_PORT);
+    mqtt_client.setCallback(mqtt_callback);
+
+    Serial.println("Try to connect to mqtt server!");
+    if (!MQTT_CLIENT_ANONYMOUS) mqtt_connected = mqtt_client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASS);
+    else mqtt_connected = mqtt_client.connect(MQTT_CLIENT_ID);
+
+    if (mqtt_connected) Serial.println("Succesfully connected to mqtt server");
+    else Serial.println("Failed to connect to mqtt server!");
+}
+
+
+/**
+ * @brief Function to connect to the internet and process any network activity
+ * 
+ * @param param
+ */
+void network_task(void *param)
+{ 
+    init_Wifi();
+    init_mqtt();
 
     while (true) {
+        if (mqtt_connected) {
 
+        }
     }
 }
 
